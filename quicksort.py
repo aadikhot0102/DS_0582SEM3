5cfc870195122b978b42e6161a56e08ec1c122fb
aadikhot0102 <112844732+aadikhot0102@users.noreply.github.com>
2022-12-20 21:13:03 +0530

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,27 @@
+
+def position(array,low high)
+pivot = array[HIGH]
+i=low-1
+for j in range (low,high)
+if array[j]<=pivot
+i = i+1
+(array[i];array[j])=(array[j],array[i])
+(array[i+1];array[high]=(array[high],array[i+1]))
+return i+1
+def quick sort[array,low,high]
+if low<high
+p1=position(array,low,high)
+quick sort(array,low,pi-1)
+quick sort(array,pi+1,high)
+data=[]
+size=int(input("size:")
+for "i" in range(size)
+elm = input(input("elm:"))
+quick sort(data)
+for "i" in range(len(size))
+print(list[i]end="")
+size=4
+ele=9
+ele=8
+elm=12
+elm=-3
